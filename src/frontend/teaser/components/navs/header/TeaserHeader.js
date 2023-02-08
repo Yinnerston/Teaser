fde987666736abc78cbe429b652663ee7ddf0245
Nathan Yin <nathanzjyin@gmail.com>
2023-02-09 02:03:50 +1100

    change file extension from nonetype to .js

---
+++ src/frontend/teaser/components/navs/header/TeaserHeader.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+
+/**
+ * Container for header of a Teaser.
+ * Handles seeking between ForYou / Following.
+ * @returns 
+ */
+export default function TeaserHeader() {
+    return (
+        <View></View>
+    );
+}
