45355b230ceb56d5d36bd97fdcb81f732d806f39
Nathan Yin <nathanzjyin@gmail.com>
2023-02-09 01:59:15 +1100

    defined nav components on top of TeaserView

--- src/frontend/teaser/components/teaser/TeaserView.js
+++ src/frontend/teaser/components/teaser/TeaserView.js
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Video } from 'expo-av';
+import TeaserCaption from "../navs/caption/TeaserCaption";
+import TeaserHeader from "../navs/header/TeaserHeader";
+import TeaserSidebar from "../navs/sidebar/TeaserSidebar";
+import TeaserVideo from "../navs/video/TeaserVideo";
 
+/**
+ *  Container for all the components that make up a teaser.
+ * @returns 
+ */
 export default function TeaserView()    {
     // const [post, setPost] = useState(props.post);
     
-
     return (
         <View style={styles.container}>
+          <TeaserHeader></TeaserHeader>
+          <TeaserVideo></TeaserVideo>
+          <TeaserSidebar></TeaserSidebar>
+          <TeaserCaption></TeaserCaption>
         </View>
     );
 }
