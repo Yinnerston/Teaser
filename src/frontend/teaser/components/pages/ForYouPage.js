11b5a4a02187bbbea7a2aa213f912029b860abf2
Nathan Yin <nathanzjyin@gmail.com>
2023-02-10 23:03:36 +1100

    delete unused element

--- src/frontend/teaser/components/pages/ForYouPage.js
+++ src/frontend/teaser/components/pages/ForYouPage.js
@@ -1,5 +1,4 @@
 import { View } from "react-native";
-import HomepageFooter from "../navs/footer/HomepageFooter";
 import TeaserView from "../teaser/TeaserView";
 
 export default function ForYouPage()    {
