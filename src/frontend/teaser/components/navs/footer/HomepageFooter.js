e35ebcb5a2794e0aa4f16c69257f5b3b90de559f
Nathan Yin <nathanzjyin@gmail.com>
2023-02-10 14:50:47 +1100

    added icons to HomepageFooter

--- src/frontend/teaser/components/navs/footer/HomepageFooter.js
+++ src/frontend/teaser/components/navs/footer/HomepageFooter.js
@@ -1,4 +1,7 @@
 import { StyleSheet, View } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons'; 
+import { SimpleLineIcons } from '@expo/vector-icons'; 
 
 /**
  * Container for footer of the homepage.
@@ -8,13 +11,17 @@ import { StyleSheet, View } from "react-native";
 export default function HomepageFooter() {
     return (
         <View style={styles.container}>
-
+            <AntDesign style={{flex: 1}} name="home" size={24} color="black" />
+            <AntDesign style={{flex: 2}} name="heart" size={24} color="black" />
+            <SimpleLineIcons style={{flex: 3}} name="speech" size={24} color="black" />
+            <Ionicons style={{flex: 4}} name="person-outline" size={24} color="black" />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#ffffff'
+        backgroundColor: '#ffffff',
+        flexDirection: 'row'
     }
 })
