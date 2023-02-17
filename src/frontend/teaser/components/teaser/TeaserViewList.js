4b9ef25cf27dde4fecb8675785a963ac2f6cfa69
Nathan Yin <nathanzjyin@gmail.com>
2023-02-17 16:55:25 +1100

    added more examples to playlist

--- src/frontend/teaser/components/teaser/TeaserViewList.js
+++ src/frontend/teaser/components/teaser/TeaserViewList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -36,19 +36,67 @@ const PLAYLIST = [
       thumbnailURL: "https://i.imgur.com/MlCqb3r.png",
     },
   },
+  {
+    data: {
+      id: "62927b9918c2868d1dc29cd355cb74f1",
+      title: "It's so sad..",
+      author: "Dr Manhatten",
+      likes: 23,
+      commentCount: 0,
+    },
+    video: {
+      videoURL: "https://i.imgur.com/6UGTKlH.mp4",
+      thumbnailURL: "https://i.imgur.com/2pt4fKv.jpeg",
+    },
+  },
+  {
+    data: {
+      id: "31fc4e5909e09bf3163be8cbacce6250",
+      title: "Sigma Female",
+      author: "Audrey Tate",
+      likes: 24,
+      commentCount: 0,
+    },
+    video: {
+      videoURL: "https://i.imgur.com/7JTRTzw.mp4",
+      thumbnailURL: "https://i.imgur.com/4ZAjB8X.png",
+    },
+  },
+  {
+    data: {
+      id: "8c4183952b01b88ac9707e34bb21ae26",
+      title: "Sharkskin Warning",
+      author: "The Expert",
+      likes: 6,
+      commentCount: 0,
+    },
+    video: {
+      videoURL: "https://i.imgur.com/OO6Yk2f.mp4",
+      thumbnailURL: "https://i.imgur.com/lhaXT6Y.png",
+    },
+  },
+  {
+    data: {
+      id: "5ef0c224edb9d7adaa6bae1c43152fb4",
+      title: "CAM ON INGERLAND",
+      author: "Marissa Touhou",
+      likes: 44,
+      commentCount: 0,
+    },
+    video: {
+      videoURL: "https://i.imgur.com/QlHUHfc.mp4",
+      thumbnailURL: "https://i.imgur.com/0mmvi7g.png",
+    },
+  },
 ];
 
 export default function TeaserViewList() {
   const [curData, setCurData] = useState({});
   const [curVideo, setCurVideo] = useState({});
   const windowDimensions = useWindowDimensions();
-  const onViewableItemsChange = ({ changed, viewableItems }) => {
-    // Set the data object
-    console.log(changed, viewableItems);
-    console.log("I am here");
-    // Set the video object
-    // Seek to the start of the video
-  };
+  const handleViewableItemsChange = useRef(({ changed, viewableItems }) => {
+    console.log("Items changed!");
+  });
 
   return (
     <SafeAreaView style={styles.container}>
@@ -62,7 +110,7 @@ export default function TeaserViewList() {
           ></TeaserView>
         )}
         keyExtractor={(item) => item.data.id}
-        onViewableItemsChange={onViewableItemsChange}
+        onViewableItemsChange={handleViewableItemsChange}
         viewabilityConfig={{
           viewAreaCoveragePercentThreshold: 60,
           waitForInteraction: true,
