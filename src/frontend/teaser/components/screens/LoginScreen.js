64753e8c373af71a00f5b03b91b0a8942a591917
Nathan Yin <nathanzjyin@gmail.com>
2023-02-20 16:41:52 +1100

    added login screen for username + password

---
+++ src/frontend/teaser/components/screens/LoginScreen.js
@@ -0,0 +1,39 @@
+import { View, StyleSheet } from "react-native";
+import { useForm } from "react-hook-form";
+
+/**
+ * Login Screen.
+ * @returns
+ */
+export default function LoginScreen() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  // TODO: Implement Login endpoint
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <View style={styles.container}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          defaultValue="Username"
+          {...register("username", { required: true })}
+        ></input>
+        {errors.username && <span>This field is required</span>}
+        <input
+          defaultValue="Password"
+          {...register("password", { required: true })}
+        ></input>
+        {errors.password && <span>This field is required</span>}
+        <input type="submit" />
+      </form>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
