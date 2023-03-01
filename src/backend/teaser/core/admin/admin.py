ba8724a88a38fe0a2b696c77a9e712c0ca34aeec
Nathan Yin <nathanzjyin@gmail.com>
2023-03-01 18:44:53 +1100

    defined TeaserUsersUsers, LocationsModel. defined admin for Users

--- src/backend/teaser/core/admin/admin.py
+++ src/backend/teaser/core/admin/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from core.models.user_auth_models import TeaserUser
 
-# Register your models here.
+
+# Inline descriptor for TeaserUser model
+class TeaserUserInline(admin.StackedInline):
+    model = TeaserUser
+    can_delete = False
+    verbose_name_plural = "teaser user"
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (TeaserUserInline,)
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- src/backend/teaser/core/models/user_auth_models.py
+++ src/backend/teaser/core/models/user_auth_models.py
@@ -1,3 +1,31 @@
 """
 Models for the user, and the related categories.
 """
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class LocationsModel(models.Model):
+    """
+    Location model.
+    """
+
+    address = models.CharField(max_length=300)
+    country_code = models.CharField(max_length=3)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+
+
+class TeaserUserModel(models.Model):
+    """
+    Teaser User model.
+    """
+
+    user_model = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=16)
+    profile_photo_url = models.URLField()
+    stage_name = models.CharField(max_length=64)
+    dob = models.DateField()
+    is_verified = models.BooleanField(default=False)
+    location_id = models.ForeignKey(LocationsModel, on_delete=models.DO_NOTHING)
