e9358f52819bc587b5a538faaeb0440c7d772117
Nathan Yin <nathanzjyin@gmail.com>
2023-03-01 20:43:45 +1100

    defined data models for event metrics and comments closure table

--- src/backend/teaser/core/models/event_metric_models.py
+++ src/backend/teaser/core/models/event_metric_models.py
@@ -1,3 +1,19 @@
 """
 Models for events from the app and various metrics derived from the event.
 """
+from core.models.user_auth_models import TeaserUserModel
+from django.db import models
+
+
+class EventMetricsTypeModel(models.Model):
+    type = models.CharField(
+        max_length=20,
+        primary_key=True,
+        help_text="Events created by a user: Clicks, watch, swipe right, etc..",
+    )
+
+
+class EventMetricsModel(models.Model):
+    user_id = models.ForeignKey(TeaserUserModel, on_delete=models.CASCADE)
+    event_type = models.ForeignKey(EventMetricsTypeModel, on_delete=models.DO_NOTHING)
+    event_data = models.JSONField()
