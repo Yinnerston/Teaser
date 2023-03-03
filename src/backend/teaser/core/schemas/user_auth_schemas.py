fda739c358bd76aeb03a0c23432ba60cece9ba2e
Nathan Yin <nathanzjyin@gmail.com>
2023-03-03 12:16:52 +1100

    defined example values for user_auth_schema

--- src/backend/teaser/core/schemas/user_auth_schemas.py
+++ src/backend/teaser/core/schemas/user_auth_schemas.py
@@ -6,9 +6,9 @@ class TeaserUserSchema(Schema):
     Schema for TeaserUser in /api/v1/register endpoint.
     """
 
-    username: str
-    email: str
-    phone: str
-    password: str
-    dob: str
-    terms_of_service_accepted: bool = Field(default=False)
+    username: str = Field(example="testuser1")
+    email: str = Field(example="testuser1@tester.com")
+    phone: str = Field(example="+61499499499")
+    password: str = Field(example="AmongUsS8SSY!FR")
+    dob: str = Field(example="21/12/1995")
+    terms_of_service_accepted: bool = Field(default=True)
