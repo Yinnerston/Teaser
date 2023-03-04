b81e7209c1e2b4983f9426920f260e2f96d92066
Nathan Yin <nathanzjyin@gmail.com>
2023-03-04 23:20:22 +1100

    fixed import of user_auth_admin, added empty admin.ModelAdmin classes for TeaserUserModel and LocationModel

---
+++ src/backend/teaser/core/admin/__init__.py
@@ -0,0 +1,1 @@
+import core.admin.user_auth_admin
--- src/backend/teaser/core/admin/user_auth_admin.py
+++ src/backend/teaser/core/admin/user_auth_admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from core.models.user_auth_models import TeaserUser
+from core.models.user_auth_models import TeaserUserModel, LocationsModel
+
+# TODO: Import this in admin --> Not in admin.py
 
 
 # Inline descriptor for TeaserUser model
 class TeaserUserInline(admin.StackedInline):
-    model = TeaserUser
+    model = TeaserUserModel
     can_delete = False
     verbose_name_plural = "teaser user"
 
@@ -14,6 +16,16 @@ class TeaserUserInline(admin.StackedInline):
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
     inlines = (TeaserUserInline,)
+
+
+@admin.register(TeaserUserModel)
+class TeaserUserModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LocationsModel)
+class LocationsModelAdmin(admin.ModelAdmin):
+    pass
 
 
 # Re-register UserAdmin
