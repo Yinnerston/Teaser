80001551f61a8f095e7edb05e9de39272e6f7631
Nathan Yin <nathanzjyin@gmail.com>
2023-03-04 23:29:34 +1100

    removed unused admin definitions

--- src/backend/teaser/core/admin/user_auth_admin.py
+++ src/backend/teaser/core/admin/user_auth_admin.py
@@ -1,33 +1,19 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
 from core.models.user_auth_models import TeaserUserModel, LocationsModel
-
-# TODO: Import this in admin --> Not in admin.py
-
-
-# Inline descriptor for TeaserUser model
-class TeaserUserInline(admin.StackedInline):
-    model = TeaserUserModel
-    can_delete = False
-    verbose_name_plural = "teaser user"
-
-
-# Define a new User admin
-class UserAdmin(BaseUserAdmin):
-    inlines = (TeaserUserInline,)
 
 
 @admin.register(TeaserUserModel)
 class TeaserUserModelAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "nfc_username",
+        "nfc_email_address",
+        "phone_str",
+        "dob_date",
+        "user_model",
+        "location_id",
+    )
 
 
 @admin.register(LocationsModel)
 class LocationsModelAdmin(admin.ModelAdmin):
-    pass
-
-
-# Re-register UserAdmin
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+    list_display = ("address", "country_code", "state", "city")
