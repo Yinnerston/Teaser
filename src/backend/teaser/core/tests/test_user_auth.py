3583d881dfe6ced4e20a0f100c2aaec0662deb3b
Nathan Yin <nathanzjyin@gmail.com>
2023-03-05 12:54:49 +1100

    wrote user auth tests

--- src/backend/teaser/core/tests/test_user_auth.py
+++ src/backend/teaser/core/tests/test_user_auth.py
@@ -3,9 +3,13 @@ from core.services.user_auth_services import register_user_service
 from core.errors.user_auth_errors import (
     PatternMatchValidationError,
     UserAlreadyExistsValidationError,
+    TermsOfServiceNotAcceptedValidationError,
+    InvalidDOBValidationError,
 )
 from core.models.user_auth_models import TeaserUserModel
 from django.contrib.auth.models import User
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class TestUserAuthRegisterService(TestCase):
@@ -88,12 +92,12 @@ class TestUserAuthRegisterService(TestCase):
         # Check user has been created
         self.assertEquals(User.objects.count(), 1)
         self.assertEquals(TeaserUserModel.objects.count(), 1)
-        # Duplicate user
+        # Duplicate user username
         with self.assertRaises(UserAlreadyExistsValidationError):
             register_user_service(
                 TestUserAuthRegisterService.register_data["username"],
-                TestUserAuthRegisterService.register_data["email"],
-                TestUserAuthRegisterService.register_data["phone"],
+                "isolateUsername@gmail.com",
+                "+61415799799",
                 TestUserAuthRegisterService.register_data["password"],
                 TestUserAuthRegisterService.register_data["dob"],
                 TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
@@ -108,6 +112,34 @@ class TestUserAuthRegisterService(TestCase):
         pass
 
     def test_register_email_invalid_length(self):
+        with self.assertRaises(PatternMatchValidationError):
+            register_user_service(
+                TestUserAuthRegisterService.register_data["username"],
+                "A" * 255 + "@gmail.com",
+                TestUserAuthRegisterService.register_data["phone"],
+                TestUserAuthRegisterService.register_data["password"],
+                TestUserAuthRegisterService.register_data["dob"],
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 0)
+        self.assertEquals(TeaserUserModel.objects.count(), 0)
+
+    def test_register_email_invalid_pattern(self):
+        # Requires "@text.text" --> Missing . for email domain
+        with self.assertRaises(PatternMatchValidationError):
+            register_user_service(
+                TestUserAuthRegisterService.register_data["username"],
+                "Test" + "@gmail",
+                TestUserAuthRegisterService.register_data["phone"],
+                TestUserAuthRegisterService.register_data["password"],
+                TestUserAuthRegisterService.register_data["dob"],
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 0)
+        self.assertEquals(TeaserUserModel.objects.count(), 0)
+        # Suffix can be any length as long as the the length is within the bounds
+
+    def test_register_email_uniqueness(self):
         register_user_service(
             TestUserAuthRegisterService.register_data["username"],
             TestUserAuthRegisterService.register_data["email"],
@@ -116,30 +148,135 @@ class TestUserAuthRegisterService(TestCase):
             TestUserAuthRegisterService.register_data["dob"],
             TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
         )
-
-    def test_register_email_invalid_pattern(self):
-        pass
-
-    def test_register_email_uniqueness(self):
-        pass
+        # Check user has been created
+        self.assertEquals(User.objects.count(), 1)
+        self.assertEquals(TeaserUserModel.objects.count(), 1)
+        # Duplicate user email
+        with self.assertRaises(UserAlreadyExistsValidationError):
+            register_user_service(
+                "isolateUsername",
+                TestUserAuthRegisterService.register_data["email"],
+                "+61415799799",
+                TestUserAuthRegisterService.register_data["password"],
+                TestUserAuthRegisterService.register_data["dob"],
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 1)
+        self.assertEquals(TeaserUserModel.objects.count(), 1)
 
     def test_register_phone_invalid_length(self):
-        pass
+        with self.assertRaises(PatternMatchValidationError):
+            register_user_service(
+                TestUserAuthRegisterService.register_data["username"],
+                TestUserAuthRegisterService.register_data["email"],
+                "+61415415415415415415",
+                TestUserAuthRegisterService.register_data["password"],
+                TestUserAuthRegisterService.register_data["dob"],
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 0)
+        self.assertEquals(TeaserUserModel.objects.count(), 0)
 
     def test_register_phone_invalid_pattern(self):
-        pass
+        # Missing country code
+        with self.assertRaises(PatternMatchValidationError):
+            register_user_service(
+                TestUserAuthRegisterService.register_data["username"],
+                TestUserAuthRegisterService.register_data["email"],
+                "0415466466",
+                TestUserAuthRegisterService.register_data["password"],
+                TestUserAuthRegisterService.register_data["dob"],
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 0)
+        self.assertEquals(TeaserUserModel.objects.count(), 0)
 
     def test_register_phone_duplicate_phone(self):
-        pass
-
-    def test_register_dob_invalid_length(self):
-        pass
+        register_user_service(
+            TestUserAuthRegisterService.register_data["username"],
+            TestUserAuthRegisterService.register_data["email"],
+            TestUserAuthRegisterService.register_data["phone"],
+            TestUserAuthRegisterService.register_data["password"],
+            TestUserAuthRegisterService.register_data["dob"],
+            TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+        )
+        # Check user has been created
+        self.assertEquals(User.objects.count(), 1)
+        self.assertEquals(TeaserUserModel.objects.count(), 1)
+        # Duplicate user phone
+        with self.assertRaises(UserAlreadyExistsValidationError):
+            register_user_service(
+                "someOtherUser",
+                "isolateUsername@gmail.com",
+                TestUserAuthRegisterService.register_data["phone"],
+                TestUserAuthRegisterService.register_data["password"],
+                TestUserAuthRegisterService.register_data["dob"],
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 1)
+        self.assertEquals(TeaserUserModel.objects.count(), 1)
 
     def test_register_dob_invalid_pattern(self):
-        pass
+        # Try abbreviated year: Requires %d/%m/%Y
+        with self.assertRaises(InvalidDOBValidationError):
+            register_user_service(
+                TestUserAuthRegisterService.register_data["username"],
+                TestUserAuthRegisterService.register_data["email"],
+                TestUserAuthRegisterService.register_data["phone"],
+                TestUserAuthRegisterService.register_data["password"],
+                "01/01/94",
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 0)
+        self.assertEquals(TeaserUserModel.objects.count(), 0)
+        # Try verbose: Requires %d/%m/%Y
+        with self.assertRaises(InvalidDOBValidationError):
+            register_user_service(
+                TestUserAuthRegisterService.register_data["username"],
+                TestUserAuthRegisterService.register_data["email"],
+                TestUserAuthRegisterService.register_data["phone"],
+                TestUserAuthRegisterService.register_data["password"],
+                "January 1st 1994",
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 0)
+        self.assertEquals(TeaserUserModel.objects.count(), 0)
+        # Try UTC: Requires %d/%m/%Y
+        with self.assertRaises(InvalidDOBValidationError):
+            register_user_service(
+                TestUserAuthRegisterService.register_data["username"],
+                TestUserAuthRegisterService.register_data["email"],
+                TestUserAuthRegisterService.register_data["phone"],
+                TestUserAuthRegisterService.register_data["password"],
+                "915148800.75",
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 0)
+        self.assertEquals(TeaserUserModel.objects.count(), 0)
 
     def test_register_dob_invalid_age(self):
-        pass
+        # get the date a week ago in the format %d/%m/%Y
+        last_week = datetime.today() - relativedelta(days=7)
+        last_week_str = last_week.strftime("%d/%m/%Y")
+        with self.assertRaises(InvalidDOBValidationError):
+            register_user_service(
+                TestUserAuthRegisterService.register_data["username"],
+                TestUserAuthRegisterService.register_data["email"],
+                TestUserAuthRegisterService.register_data["phone"],
+                TestUserAuthRegisterService.register_data["password"],
+                last_week_str,
+                TestUserAuthRegisterService.register_data["terms_of_service_accepted"],
+            )
+        self.assertEquals(User.objects.count(), 0)
+        self.assertEquals(TeaserUserModel.objects.count(), 0)
 
     def test_register_terms_of_service_accepted(self):
-        pass
+        with self.assertRaises(TermsOfServiceNotAcceptedValidationError):
+            register_user_service(
+                TestUserAuthRegisterService.register_data["username"],
+                TestUserAuthRegisterService.register_data["email"],
+                TestUserAuthRegisterService.register_data["phone"],
+                TestUserAuthRegisterService.register_data["password"],
+                TestUserAuthRegisterService.register_data["dob"],
+                False,
+            )
