2e6811ed0358885cff925bc894cc62ba0cba55bf
Nathan Yin <nathanzjyin@gmail.com>
2023-04-06 19:53:01 +1000

    Added class for sound and defined r/w/wr atoms for sound

--- src/frontend/teaser/hooks/upload/useSound.js
+++ src/frontend/teaser/hooks/upload/useSound.js
@@ -1,7 +1,53 @@
 import { atom } from "jotai";
+import { msToWidth } from "../../utils/videoTimelineWidth";
+class SoundClip {
+  constructor(id, title, sound, author, thumbnail) {
+    this.key = id; // no overlap with video q node?
+    this.title = title;
+    this.sound = sound;
+    this.author = author;
+    this.thumbnail = thumbnail;
+    this.startTimeMs = 0;
+    // Variables to reduce computation
+    this.startTimeWidth = 0;
+    this.endTimeMs = 0;
+    this.endTimeWidth = 0;
+    console.log(sound);
+    this.durationWidth = msToWidth(sound.duration);
+  }
 
-export const editorSoundAtom = atom(null);
+  setStartTimeMs(ms) {
+    if (ms > 0) {
+      ms = ms + 1; // Add 0.1ms offset from the prev video
+    }
+    this.startTimeMs = ms;
+    this.startTimeWidth = msToWidth(ms);
+    let endTimeMs = this.startTimeMs + this.sound.duration;
+    this.endTimeMs = endTimeMs;
+    this.endTimeWidth = msToWidth(endTimeMs);
+    // TODO: Check if enqueued atom exceeds the maximum duration of a video?
+  }
+  // TODO: change the duration of a sound clip
+}
 
-export const editorHasAddedSoundAtomAtom = atom(
-  (get) => get(editorSoundAtom) != null,
+const _editorSoundAtom = atom(null);
+
+export const readOnlyEditorSoundAtomAtom = atom((get) => get(_editorSoundAtom));
+
+export const writeOnlyEditorSoundAtomAtom = atom(null, (get, set, update) => {
+  const { id, title, sound, author, thumbnail } = update;
+  let newSoundClip = new SoundClip(id, title, sound, author, thumbnail);
+  set(_editorSoundAtom, newSoundClip);
+});
+
+export const readWriteEditorSoundAtomAtom = atom(
+  (get) => get(_editorSoundAtom),
+  (get, set, update) => {
+    let newSoundClip = new SoundClip(update);
+    set(_editorSoundAtom, newSoundClip);
+  },
+);
+
+export const editorSoundIsSetAtomAtom = atom(
+  (get) => get(_editorSoundAtom) != null,
 );
